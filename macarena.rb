6e567da40b4ea1f0ac75483d635b2fc9a20f0614
cdeocampo2001 <sweetcaroline601@gmail.com>
2018-07-23 23:07:35 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,30 +1,13 @@
-puts "How To Do The Macarena:"
-puts ""
-
-# Fix The Dance Instructions Below
-# Every line should have one instruction using puts.
-# The first instruction is correct. 
-
-# Step 1: GOOD
-puts "Step 1: Right arm out in front of you, palm facing down."
-
-# Step 2: FIXME
-puts "Step 2: Turn your right palm up."
-
-# Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
-
-# Step 4: FIXME
-puts "Step 4: Turn your left palm up."
-
-# Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
-
-# Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
-
-# Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
-
-# Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Put your right arm out"
+puts "Put your left arm out"
+puts "Flip right palm"
+puts "Flip left palm"
+puts "Right arm, left shoulder"
+puts "Left arm, right shoulder"
+puts "Right arm to your head"
+puts "Left arm to your head"
+puts "Right arm, left hip"
+puts "Left arm, right hip"
+puts "Right arm, right hip"
+puts "Left arm, left hip"
+puts "ayyyyyyyyyy macarena(turn)"
